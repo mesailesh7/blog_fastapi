e24100ea995c392365bc90f99e6ca63f7d97eb22
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2026-09-05 23:58:06 -0600

    Route Names

--- backend/main.py
+++ backend/main.py
@@ -52,8 +52,8 @@ posts: list[dict] = [
 # def home():
 #     return f"<h1>{posts[0]['title']}</h1>"
 
-@app.get("/",  include_in_schema=False)
-@app.get("/posts", include_in_schema=False)
+@app.get("/",  include_in_schema=False, name="home")
+@app.get("/posts", include_in_schema=False, name="posts")
 def home(request: Request):
     return templates.TemplateResponse(request, "home.html", {"posts": posts})
 
