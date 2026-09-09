41be65d2347a7f627265b717d92aa95de42f4271
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2026-09-08 23:52:55 -0600

    Error Startlet HttpException

--- backend/main.py
+++ backend/main.py
@@ -61,7 +61,7 @@ def home(request: Request):
 
 
 @app.get("/posts/{post_id}", include_in_schema=False)
-def post_paget(request: Request,post_id: int):
+def post_page(request: Request,post_id: int):
     for post in posts:
         if post.get("id") == post_id:
             title = post['title'][:50]
@@ -90,4 +90,29 @@ def get_post(post_id: int):
     return HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Post not found"
+    )
+
+@app.exception_handler(StarletteHTTPException)
+def general_http_exception_handler(request: Request, exception: StarletteHTTPException):
+    message = (
+        exception.detail
+        if exception.detail
+        else "An unhandled exception occurred."
+    )
+
+    if request.url.path.startswith("/api/"):
+        return JSONResponse(
+            status_code=exception.status_code,
+            content={"message": message},
+        )
+
+    return templates.TemplateResponse(
+        request,
+        "error.html",
+        {
+            "status_code": exception.status_code,
+            "title": exception.status_code,
+            "message": message,
+        },
+        status_code=exception.status_code,
     )
